d1bb51da0926dc73c0cd92dd80866cd228a616c1
Rakshann761 <175554879+Rakshann761@users.noreply.github.com>
2025-12-01 18:29:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from product_list import loadplist
-import numpy
+import numpy as np
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
 from streamlit_webrtc import AudioProcessorBase
-
 import av
 
 
